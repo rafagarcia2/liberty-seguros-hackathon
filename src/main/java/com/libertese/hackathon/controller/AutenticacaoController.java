7810521579f88bf17d596676218749313ce99295
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2019-02-14 20:40:00 -0200

    Autenticação front.

--- src/main/java/com/libertese/hackathon/controller/AutenticacaoController.java
+++ src/main/java/com/libertese/hackathon/controller/AutenticacaoController.java
@@ -2,13 +2,20 @@ package com.libertese.hackathon.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/autenticacao")
 public class AutenticacaoController {
 
-    @GetMapping("/autenticacao/login")
+    @GetMapping("/login")
     public String login() {
         return "autenticacao/login";
+    }
+
+    @GetMapping("/cadastro")
+    public String cadastro(){
+        return "autenticacao/cadastro";
     }
 
 }
