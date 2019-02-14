1261555a622d06a6b4f1bcc20443366ee64193e6
gabrielArSouza <gabriel_feg@hotmail.com>
2019-02-14 21:14:39 -0200

    rota para cadastrar cliente

--- src/main/java/com/libertese/hackathon/controller/ClientController.java
+++ src/main/java/com/libertese/hackathon/controller/ClientController.java
@@ -2,7 +2,12 @@ package com.libertese.hackathon.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.libertese.hackathon.model.Client;
 
 
 @Controller
@@ -15,4 +20,19 @@ public class ClientController {
     public String cadastro() {
         return "cliente/create";
     }
+	
+	@PostMapping("create")
+	public String create (@ModelAttribute("client") Client client, 
+			RedirectAttributes redirectAtrributes)
+	{
+		try {
+			//TODO tenta inserir cliente
+			
+			// mensagem de sucesso
+			redirectAtrributes.addFlashAttribute("sucesso", INSERT_SUCCESS);
+		}  catch (Exception e) {
+			redirectAtrributes.addFlashAttribute("erro", e.getMessage());
+		}
+		return "redirect:/cliente/index";
+	}
 }
