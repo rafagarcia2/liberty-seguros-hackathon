04ec8fe14f2761de616991e452f71b6ebd224b06
Mayra Azevedo <mayradazevedo@gmail.com>
2019-02-14 19:33:37 -0200

    fix css import

--- src/main/java/com/libertese/hackathon/SecurityConfig.java
+++ src/main/java/com/libertese/hackathon/SecurityConfig.java
@@ -34,8 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/static/*").permitAll()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/static/**").permitAll()
                 .antMatchers("/view/plugins**").permitAll()
                 .antMatchers("/view/**").permitAll()
                 .antMatchers("/user-register**").permitAll()
