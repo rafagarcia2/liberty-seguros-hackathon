4bc50ca7b0314f6b967c32f38ef39d9ea5f0da21
Mayra Azevedo <mayradazevedo@gmail.com>
2019-02-14 17:09:24 -0200

    initial commit

---
+++ src/main/java/com/libertese/hackathon/LiberteSeApplication.java
@@ -0,0 +1,14 @@
+package com.libertese.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberteSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberteSeApplication.class, args);
+	}
+
+}
+
