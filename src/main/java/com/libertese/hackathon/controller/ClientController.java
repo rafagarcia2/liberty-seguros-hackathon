cf7183c7280e15724f99795b6aa46807ce775ae8
gabrielArSouza <gabriel_feg@hotmail.com>
2019-02-15 18:12:10 -0200

    tenta o casatro de cliente

--- src/main/java/com/libertese/hackathon/controller/ClientController.java
+++ src/main/java/com/libertese/hackathon/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.libertese.hackathon.controller;
 
+import com.libertese.hackathon.model.Address;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +19,10 @@ public class ClientController {
 	private static final String INSERT_SUCCESS = "Cliente inserido com sucesso!";
 	
 	@GetMapping("create")
-    public String cadastro() {
-        return "cliente/create";
+    public String cadastro(Model m) {
+		Address ad = new Address(null, null, null, null, null, null, null);
+		m.addAttribute("client", new Client(null, null, null, null, ad));
+		return "cliente/create";
     }
 
     @GetMapping("index")
@@ -30,11 +34,12 @@ public class ClientController {
 	{
 		try {
 			//TODO tenta inserir cliente
-			
+
 			// mensagem de sucesso
 			redirectAtrributes.addFlashAttribute("sucesso", INSERT_SUCCESS);
 		}  catch (Exception e) {
 			redirectAtrributes.addFlashAttribute("erro", e.getMessage());
+			return "redirect:/cliente/create";
 		}
 		return "redirect:/cliente/create";
 	}
