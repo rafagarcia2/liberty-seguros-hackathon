059b7555b67b64fbee1cc70335b8a0e8681c30c2
gabrielArSouza <gabriel_feg@hotmail.com>
2019-02-15 19:36:46 -0200

    encontrar erro de inserção

--- src/main/java/com/libertese/hackathon/controller/ClientController.java
+++ src/main/java/com/libertese/hackathon/controller/ClientController.java
@@ -1,6 +1,5 @@
 package com.libertese.hackathon.controller;
 
-import com.libertese.hackathon.model.Address;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
