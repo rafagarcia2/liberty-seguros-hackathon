3ce9b1ea250611d7734e12076c581ab8a3db2d9c
Rafael Garcia <rafaelgarcia2013@gmail.com>
2019-02-16 01:41:55 -0200

    #7 Tabelas de Cotacao

---
+++ src/main/java/com/libertese/hackathon/model/Cotacao.java
@@ -0,0 +1,104 @@
+package com.libertese.hackathon.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="cotacao")
+public class Cotacao {
+
+	@Id 
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_COTACAO")
+	@SequenceGenerator(name="SEQ_COTACAO", sequenceName="id_seq_cotacao", allocationSize=1)
+	@Column(name="id_cotacao")
+	private int id;
+	
+	@Column(name="id_cliente")
+	private Client cliente;
+	
+	@Column(name="id_segurado")
+	private Seguradora seguradora;
+	
+	@Column
+	private String ramo;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="vigencia_inicio", columnDefinition="DATE")
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private Date inicioVigencia;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="vigencia_fim", columnDefinition="DATE")
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private Date fimVigencia;
+
+	
+	public Cotacao(int id, Client cliente, String ramo, Date inicioVigencia, Date fimVigencia) {
+		super();
+		this.id = id;
+		this.cliente = cliente;
+		this.ramo = ramo;
+		this.inicioVigencia = inicioVigencia;
+		this.fimVigencia = fimVigencia;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Client getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Client cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getRamo() {
+		return ramo;
+	}
+
+	public void setRamo(String ramo) {
+		this.ramo = ramo;
+	}
+
+	public Date getInicioVigencia() {
+		return inicioVigencia;
+	}
+
+	public void setInicioVigencia(Date inicioVigencia) {
+		this.inicioVigencia = inicioVigencia;
+	}
+
+	public Date getFimVigencia() {
+		return fimVigencia;
+	}
+
+	public void setFimVigencia(Date fimVigencia) {
+		this.fimVigencia = fimVigencia;
+	}
+
+	public Seguradora getSeguradora() {
+		return seguradora;
+	}
+
+	public void setSeguradora(Seguradora seguradora) {
+		this.seguradora = seguradora;
+	}
+	
+}
---
+++ src/main/java/com/libertese/hackathon/model/VeiculoInformacoes.java
@@ -0,0 +1,155 @@
+package com.libertese.hackathon.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="veiculo_informacoes")
+public class VeiculoInformacoes {
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_VEICULO")
+	@SequenceGenerator(name="SEQ_VEICULO", sequenceName="id_seq_veiculo", allocationSize=1)
+	@Column(name="id_veiculo")
+	private int id;
+	
+	@Column(name="id_cotacao")
+	private Cotacao cotacao;
+	
+	@Column
+	private String placa;
+	
+	@Column
+	private String chassi;
+	
+	@Column
+	private String marca;
+	
+	@Column
+	private int ano_modelo;
+	
+	@Column
+	private String tipo_combustivel;
+	
+	@Column
+	private String tipo_veiculo;
+	
+	@Column
+	private boolean zero_km;
+	
+	@Column
+	private boolean acidente;
+	
+	@Column
+	private boolean brindado;
+
+	
+	public VeiculoInformacoes(Cotacao cotacao, String placa, String chassi, String marca, int ano_modelo,
+			String tipo_combustivel, String tipo_veiculo, boolean zero_km, boolean acidente, boolean brindado) {
+		super();
+		this.cotacao = cotacao;
+		this.placa = placa;
+		this.chassi = chassi;
+		this.marca = marca;
+		this.ano_modelo = ano_modelo;
+		this.tipo_combustivel = tipo_combustivel;
+		this.tipo_veiculo = tipo_veiculo;
+		this.zero_km = zero_km;
+		this.acidente = acidente;
+		this.brindado = brindado;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Cotacao getCotacao() {
+		return cotacao;
+	}
+
+	public void setCotacao(Cotacao cotacao) {
+		this.cotacao = cotacao;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getChassi() {
+		return chassi;
+	}
+
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAno_modelo() {
+		return ano_modelo;
+	}
+
+	public void setAno_modelo(int ano_modelo) {
+		this.ano_modelo = ano_modelo;
+	}
+
+	public String getTipo_combustivel() {
+		return tipo_combustivel;
+	}
+
+	public void setTipo_combustivel(String tipo_combustivel) {
+		this.tipo_combustivel = tipo_combustivel;
+	}
+
+	public String getTipo_veiculo() {
+		return tipo_veiculo;
+	}
+
+	public void setTipo_veiculo(String tipo_veiculo) {
+		this.tipo_veiculo = tipo_veiculo;
+	}
+
+	public boolean isZero_km() {
+		return zero_km;
+	}
+
+	public void setZero_km(boolean zero_km) {
+		this.zero_km = zero_km;
+	}
+
+	public boolean isAcidente() {
+		return acidente;
+	}
+
+	public void setAcidente(boolean acidente) {
+		this.acidente = acidente;
+	}
+
+	public boolean isBrindado() {
+		return brindado;
+	}
+
+	public void setBrindado(boolean brindado) {
+		this.brindado = brindado;
+	}
+
+}
